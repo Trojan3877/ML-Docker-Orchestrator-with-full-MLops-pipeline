7731b1fdc5aa4bc7c7bae7a788a38a1548aa7218
Corey Leath <corey22blue@hotmail.com>
2026-02-26 14:43:21 -0500

    Add logging configuration setup

---
+++ src/orchestrator/logging_config.py
@@ -0,0 +1,17 @@
+import logging
+from pythonjsonlogger import jsonlogger
+
+from .config import settings
+
+
+def configure_logging() -> None:
+    logger = logging.getLogger()
+    logger.setLevel(settings.log_level)
+
+    handler = logging.StreamHandler()
+    formatter = jsonlogger.JsonFormatter(
+        "%(asctime)s %(levelname)s %(name)s %(message)s %(pathname)s %(lineno)d"
+    )
+    handler.setFormatter(formatter)
+
+    logger.handlers = [handler]
